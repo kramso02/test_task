0507f71db164dc4f31cdc4592affdd8d114e9110
Taya <kramso02@mail.ru>
2026-04-28 16:34:54 +0500

    - create migrations

--- store/api/models.py
+++ store/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Item(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
