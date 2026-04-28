25f8e9735cbf880c6efeeb5077b50cf87a6fa7f0
Taya <kramso02@mail.ru>
2026-04-28 17:30:15 +0500

    - create .env.example file, install stripe and create get_checkout_session view

--- store/api/views.py
+++ store/api/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+import stripe
+import os
 
-# Create your views here.
+stripe.api_key = os.getenv('STRIPE_API_KEY')
+
+
+def get_checkout_session():
+    return stripe.checkout.Session.create(
+        payment_method_types=['card'],
+        line_items=[{
+            'price_data': {
+                'currency': 'usd',
+                'product_data': {'name': 'product'},
+                'unit_amount': 10,
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url='http://localhost:8000/success/',
+        cancel_url='http://localhost:8000/cancel/',
+    )
+
